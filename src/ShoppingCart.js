bea98dbb24994548123ef98eaf7a725b63fce729
abhinew <abhinaya.sv@gmail.com>
2018-07-14 08:35:53 +0200

    initialised items property and implemented getitems and additem function

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,6 +1,17 @@
 class ShoppingCart {
     constructor() {
-
+        this.items = []
+    }
+    getItems() {
+        return this.items
+    }
+    addItem(itemName,quantity,pricePerUnit) {
+        const item = { 
+            name: itemName,
+            quantity: quantity,
+            pricePerUnit: pricePerUnit
+        }
+        return this.items.push(item)
     }
 }
 module.exports = ShoppingCart
