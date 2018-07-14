7ae9a7f72868febeb015c750885237348936db72
abhinew <abhinaya.sv@gmail.com>
2018-07-14 15:43:35 +0200

    modified function to access the nested object's value

--- src/pathFind.js
+++ src/pathFind.js
@@ -1,18 +1,17 @@
 //input :  propertyname as an array in the function
 // for every element  in the array get the property of the object 
+//loop through the elements of the array and find the property in the object
+
 
 
 function pathFind(propertiesList, object) {
-    const property = propertiesList[0]
-    return object[property];
+    return propertiesList.reduce(function(acc,current) {
+        return acc[current];
+    },object)
+    
 }
 
 
-const obj = {
-    foo: "Hey"
-}
-
-console.log(pathFind(["foo"], obj));
 
 
 module.exports =  { pathFind }
