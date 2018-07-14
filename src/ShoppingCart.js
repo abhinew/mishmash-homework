b0dd609d73bce59e035065c6461f87e7032c9199
abhinew <abhinaya.sv@gmail.com>
2018-07-14 19:36:08 +0200

    organised the code

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -1,9 +1,9 @@
 class ShoppingCart {
     constructor() {
-        this.items = []
+        this.items = [];
     }
     getItems() {
-        return this.items
+        return this.items;
     }
     addItem(itemName,quantity,pricePerUnit) {
         const item = { 
@@ -17,22 +17,9 @@ class ShoppingCart {
         return this.items =[];
     }
     clone() {
-        const copy = Object.create(this)
-        
+        const copy = Object.create(this)       
         return copy;
     }
 }
-
-// const cart1 = new ShoppingCart()
-// cart1.addItem("Rolex watch", 2, 250.1)
-// const cart2 = cart1.clone()
-// //console.log(cart2, " is cart 2");
-// console.log("cart1", cart1.getItems())
-// console.log("cart2", cart2.getItems())
-// cart2.addItem("Shoes", 1, 90.0)
-// console.log(cart1.getItems())
-
-
-
 
 module.exports = ShoppingCart
--- src/pathFind.js
+++ src/pathFind.js
@@ -2,16 +2,11 @@
 // for every element  in the array get the property of the object 
 //loop through the elements of the array and find the property in the object
 
-
-
 function pathFind(propertiesList, object) {
     return propertiesList.reduce(function(acc,current) {
         return acc[current];
-    },object)
+    },object);
     
 }
-
-
-
 
 module.exports =  { pathFind }
