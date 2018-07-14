28661152d7fcd8cd1ed2d9a46f7d2a873da29a16
abhinew <abhinaya.sv@gmail.com>
2018-07-14 11:21:23 +0200

    implemented clone function to create copy of the object

--- src/ShoppingCart.js
+++ src/ShoppingCart.js
@@ -10,17 +10,29 @@ class ShoppingCart {
             name: itemName,
             quantity: quantity,
             pricePerUnit: pricePerUnit
-        }
-        return this.items.push(item)
+        };
+        return this.items.push(item);
     }
     clear() {
-        return this.items =[]
+        return this.items =[];
     }
     clone() {
         const copy = Object.create(this)
-        return copy
+        
+        return copy;
     }
 }
+
+// const cart1 = new ShoppingCart()
+// cart1.addItem("Rolex watch", 2, 250.1)
+// const cart2 = cart1.clone()
+// //console.log(cart2, " is cart 2");
+// console.log("cart1", cart1.getItems())
+// console.log("cart2", cart2.getItems())
+// cart2.addItem("Shoes", 1, 90.0)
+// console.log(cart1.getItems())
+
+
 
 
 module.exports = ShoppingCart
